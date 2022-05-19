332ca6fdb4f8c7df366cdb61bceab0943e2b36c4
Elena Tchakarova <105879345+etchakarova@users.noreply.github.com>
2022-05-19 21:29:55 +0300

    Add files via upload

---
+++ Lab - Functions/repeatString.js
@@ -0,0 +1,9 @@
+function repeatString(word, num) {
+    let newWord = ''
+    for (let i = 0; i < num; i++) {
+        newWord += word
+    }
+    return newWord
+}
+console.log(repeatString("abc", 3));
+console.log(repeatString("String", 2));
