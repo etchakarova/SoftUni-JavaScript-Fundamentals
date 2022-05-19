b14521d8e9d68f5d6b426c91fbcc9d112e2c6f55
Elena Tchakarova <105879345+etchakarova@users.noreply.github.com>
2022-05-19 21:34:10 +0300

    Add files via upload

---
+++ Lab - Regular Expressions/matchPhoneNumber.js
@@ -0,0 +1,12 @@
+function matchPhoneNumber(input) {
+    let text = input[0]
+    let pattern = /(?:\s|^)\+359( |-)2\1\d{3}\1\d{4}\b/g;
+    let match = pattern.exec(text);
+    let arr = [];
+    while (match != null) {
+        arr.push(match[0].trim());
+        match = pattern.exec(text);
+    }
+    console.log(arr.join(", "))
+}
+matchPhoneNumber(["+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222"]);
